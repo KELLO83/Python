06eff675b83d644c402f38bf2fd1593b5903b5b8
KELLO83 <skyblizzard64@gmail.com>
2022-03-22 19:53:08 +0900

    Hash 작업중...

--- python/Data_structures/Hash_table.py
+++ python/Data_structures/Hash_table.py
@@ -18,6 +18,7 @@
 #Hash function //10
 #10개의 hash table circle list
 
+import random
 class Hash(object):
     def __init__(self):
         self.Hash_table=[None for _ in range(10)] #10칸의 hash table
@@ -28,47 +29,55 @@ class Hash(object):
     def set(self,key,value=None): 
         #key 값이 h에 있으면 value를 update
         #key 값이 H에 없으면 (Key,value)을 insert
+        if key==None:
+            return None
         i=self.find_slot(key)
         value=self.Hash_function(key)
-        if i==False:
+        if i==False and i!=0:
             print("Hash talbe full Not find key")
             #Hash_table의 용량을 증가시켜야한다 저장할공간이 없기떄문에->만들어야함
             return False #실패
         else:
             if self.Hash_table[i]!=None:
-                self.Hash_table[i]==value #key값 업데이트 기존에있던값대신 새로운값을 넣는다
-                return value
+                self.Hash_table[i]==key #key값 업데이트 기존에있던값대신 새로운값을 넣는다
+                return key
             
             elif self.Hash_table[i]==None: #Hash_table[i]에 값이 엀을경우
-                self.Hash_table[i]=value
-                return value   
+                self.Hash_table[i]=key
+                return key   
                 
 
 
 
     def remove(self,key):
-        i=self.find_slot(key)
-        if self.Hash_table[i]==None: #삭제할 value가 존재하지않는다
-            return None
+        current_slot=self.find_slot(key) #i=현재 key값이 저장되어있는 슬롯번호
+        if self.Hash_table[current_slot]==None: #삭제할 value가 존재하지않는다
+            print("{}slot empty key".format(current_slot))
+            return False
         else: 
-            j=i
-            while True:
-                self.Hash_table[i]=None #지우자고하는 해당 slot의 value를 삭제한다
-                while True: #hash떙기기
-                    j=(j+1)%len(self.Hash_talbe)
-                    if self.Hash_table[j]==None:
-                        return key
-                    else:
-                        k=self.Hash_function(key)
+            find_slot=current_slot # i슬롯번호에 저장되어있는 키값을 지운후 그슬롯번호를 j에 저장한다 if i=5 j=5번쨰 슬롯
+       
+        self.Hash_table[current_slot]=None #지우고자하는 current_slot 의 현재 key값을 제거한다
+        while True:
+                       
+            while True: #hash떙기기
+                find_slot=(find_slot+1)%len(self.Hash_table) #find_slot의 슬롯번호를 한칸씩 전진시킨다 
+                if self.Hash_table[find_slot]==None: #만약 find_slot가 empty라면 정렬 종료
+                    return True
+                else:
+                    orgin_slot=self.Hash_function(self.Hash_table[find_slot]) #orgin_slot은 원래 key값이 저장되어햘 슬롯 번호 
+                    if (orgin_slot<current_slot<=find_slot) or (current_slot<find_slot<orgin_slot) or (find_slot<orgin_slot<current_slot): #k=원래 key값이 저장되어야할 번호 
+                        break
                         
-                        if (k<i<=j) or (i<j<k) or (j<k<i):
-                            break
-                        
-                self.Hash_table[i]==self.Hash_table[j]
-                i=j
-                
-
+        
+            self.Hash_table[current_slot]=self.Hash_table[find_slot]
+            self.Hash_table[find_slot]=None
+            current_slot=find_slot
             
+            
+
+
+        
     
     def find_slot(self,key):  #완성
         #key값이 있으면 slot번호 리턴 없으면 그 key값이 삽입될 slot 번호를 return한다
@@ -79,7 +88,7 @@ class Hash(object):
             # 1.Hash_table에 empty가있을경우
             # 2.Hash_table  full상태이지만 find key를 했을경우
             if self.Hash_table[i]!=key:
-                i=i+1%len(Hash_table)
+                i=((i+1)%len(self.Hash_table))
                 if i==start:#한바퀴 다돌음
                     return False #Hash_table이 full 이다
             elif self.Hash_table[i]==key:
@@ -87,12 +96,51 @@ class Hash(object):
                 break
         return i #i=Hash_table의 슬롯 번호
     
-    def Hash_function(self,value):
-        value=value%10
+    def Hash_function(self,key):
+        value=key%10
         return value
+    
+    def detail(self):
+        print(self.Hash_table)
+        # for i in range(len(self.Hash_table)):
+        #     print(self.Hash_table[i])
         
-        
-            
+    def clear(self):
+        self.Hash_table.clear()
+        self.Hash_table=[None for _ in range(10)]
 if __name__=="__main__":
-    hash=Hash()
-    hash.set(5)
+    Hash=Hash()
+    # random.seed(100)
+    # for _ in range(8):
+    #     insert=Hash.set(random.randrange(0,100))
+    
+    # Hash.set(98)
+    # Hash.set(108)
+    # Hash.set(18)
+    # Hash.detail()
+    # Hash.set(28)
+    # Hash.set(38)
+    # Hash.detail()
+    # Hash.remove(28)
+    # Hash.detail()
+
+    init=[0,None,2,3,22,5,55,32,None,9]
+    
+    for i in init:
+        Hash.set(i)
+        
+        
+    Hash.detail()
+    Hash.remove(3)
+    Hash.detail()
+    
+    
+    
+    
+        
+    
+    
+    
+    
+    
+        
