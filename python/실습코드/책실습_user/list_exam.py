dae9dc12ca778cddc7deef1355d515c800a588e5
KELLO83 <skyblizzard64@gmail.com>
2022-10-10 19:22:37 +0900

    no message

---
+++ python/실습코드/책실습_user/list_exam.py
@@ -0,0 +1,33 @@
+list03=['A','B','C']
+list05=list('ABC')
+list06=list(['ABC']) #이것만 ['ABC']만들어진다
+list07=list(['A','B','C'])
+list08=list(('ABC'))
+list09=list(('A','B','C'))
+
+
+print(list03)
+print(list05)
+print(list06)
+print(list07)
+print(list08)
+print(list09)
+
+#튜플은 원소를 한개씩만 저장한다
+tuple01=()
+tuple02=1,
+tuple03=(1,)
+tuple04=1,2,3
+tuple05=(1,2,3)
+tuple06='A','B','C'
+tuple07=tuple('KELLO')
+tuple08='ABC', #tuple이없을시 abc를 한곳에 담는다
+ 
+print(tuple01)
+print(tuple02)
+print(tuple03)
+print(tuple04)
+print(tuple05)
+print(tuple06)
+print(tuple07)
+print(tuple08)
